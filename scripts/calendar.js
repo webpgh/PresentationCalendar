14515a2b0acf8b56ed2295a61e886dda5c0ca0b1
Halmi Hodzhov <hhodz@softserveinc.com>
2020-02-07 10:51:02 +0200

    Get info of the logged user

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,3 +1,20 @@
-function fn1() {
-    console.log('ko sta we');
+(function () {
+
+    var currentUserCookie = getCookie("currentUser");
+
+    var currentUser = JSON.parse(cookie);
+
+    console.log('kostawe');
+    debugger;
+
+})();
+
+function getCookie(name) {
+    var pattern = RegExp(name + "=.[^;]*")
+    matched = document.cookie.match(pattern)
+    if (matched) {
+        var cookie = matched[0].split('=')
+        return cookie[1]
+    }
+    return false
 }
--- scripts/login.js
+++ scripts/login.js
@@ -58,12 +58,19 @@ function login(url, settings) {
         .then(data => load(data))
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function load(response) {
     if (response.success) {
         var userData = response.data;
         var userCookie = JSON.stringify(userData);
-        document.cookie = userCookie;
-        debugger;
+        setCookie("currentUser", userCookie, 365);
+        // document.cookie = userCookie;
         window.location = '../html/calendar.html';
     } else {
         errors.innerHTML = response.data;
